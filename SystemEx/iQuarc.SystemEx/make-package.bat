2a5f6744dec9dfc4057ad001618964074d41b55c
crissian <crissian@gmail.com>
2014-05-20 16:36:24 +0300

    changed nuget config

--- SystemEx/iQuarc.SystemEx/make-package.bat
+++ SystemEx/iQuarc.SystemEx/make-package.bat
@@ -1,2 +1,2 @@
 c:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe iQuarc.SystemEx.csproj /p:Configuration=Release
-\nuget pack iQuarc.SystemEx.csproj -Prop Configuration=Release
+nuget pack iQuarc.SystemEx.csproj -Prop Configuration=Release
