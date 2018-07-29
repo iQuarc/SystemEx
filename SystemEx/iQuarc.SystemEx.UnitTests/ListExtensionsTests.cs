a11ed743f9b7005807bceadcc0967a7ae3c75dde
Ionut Tamas <tamash_ionut@hotmail.com>
2018-07-29 12:14:43 -0700

    Added DistinctBy and BinarySearchBy extensions

---
+++ SystemEx/iQuarc.SystemEx.UnitTests/ListExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace iQuarc.SystemEx.UnitTests
+{
+    public class ListExtensionsTests {
+        [Fact]
+        public void BinarySearchBy_FromObjectEnumerable_GetsCorrectIndex()
+        {
+            var collection = new[]
+            {
+                new {Name = "Cristi", Age = 30},
+                new {Name = "Cata", Age = 20},
+                new {Name = "Ionut", Age = 19}
+            };
+
+            var index = collection.ToList().BinarySearchBy(new {Name = "", Age = 20}, x => x.Age);
+            Assert.Equal(1, index);
+        }
+    }
+}
---
+++ SystemEx/iQuarc.SystemEx/ListExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace iQuarc.SystemEx
+{
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Performs a binary search by a specific Func selector.
+        /// </summary>
+        public static int BinarySearchBy<T>(this List<T> list, T item, Func<T, IComparable> selector)
+        {
+            return list.BinarySearch(item, new FuncComparer<T>(selector));
+        }
+
+        private class FuncComparer<T> : IComparer<T> {
+            private readonly Func<T, IComparable> comparer;
+
+            public FuncComparer(Func<T, IComparable> comparer) {
+                this.comparer = comparer;
+            }
+
+            public int Compare(T x, T y)
+            {
+                return comparer(x).CompareTo(comparer(y));
+            }
+        }
+    }
+}
